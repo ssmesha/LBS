991c7bf14e07184ec19e5a6a22743c51b57ffad8
Just Alex <ainapetrova104@gmail.com>
2024-10-05 14:13:02 +0900

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,22 @@
 import SearchComponent from "../Search";
 
+
 const Home = () => {
+  const Logo = require('../Assets/WhatsApp Image 2024-10-05 at 13.10.23.jpeg')
+
   return(
     <div>
       <SearchComponent />
       Home
+
+      <img 
+        scr={Logo}
+        height="300"
+        width="300"
+        className=""
+        alt='photo'
+
+      />
     </div>
   );
 }
